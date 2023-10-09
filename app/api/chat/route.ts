5844ef121f52212989844ed2432b06346a938861
Justin Newman <newma4jm@dukes.jmu.edu>
2023-10-09 10:59:46 -0400

    Reduced code and implemented preprompting on server side.

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,46 +1,50 @@
-import { kv } from '@vercel/kv'
-import { OpenAIStream, StreamingTextResponse } from 'ai'
-import { Configuration, OpenAIApi } from 'openai-edge'
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { Configuration, OpenAIApi } from 'openai-edge';
+import { nanoid } from '@/lib/utils';
 
-import { auth } from '@/auth'
-import { nanoid } from '@/lib/utils'
-
-export const runtime = 'edge'
+export const runtime = 'edge';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
-})
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
-const openai = new OpenAIApi(configuration)
+const openai = new OpenAIApi(configuration);
 
 export async function POST(req: Request) {
-  const json = await req.json()
-  const { messages, previewToken } = json
-  const userId = (await auth())?.user.id
+  const json = await req.json();
+  const { messages, previewToken } = json;
+  const userId = 100;
+  const prePrompt = "Hello, you are now the wise old tech guru. You will respond with inspiring wisdom and quotes to help motivate. Here is your prompt:";
 
   if (!userId) {
     return new Response('Unauthorized', {
-      status: 401
-    })
+      status: 401,
+    });
   }
 
   if (previewToken) {
-    configuration.apiKey = previewToken
+    configuration.apiKey = previewToken;
   }
+
+  // Add the pre-prompt message to the messages array
+  messages.push({
+    role: 'system',
+    content: prePrompt,
+  });
 
   const res = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages,
     temperature: 0.7,
-    stream: true
-  })
+    stream: true,
+  });
 
   const stream = OpenAIStream(res, {
     async onCompletion(completion) {
-      const title = json.messages[0].content.substring(0, 100)
-      const id = json.id ?? nanoid()
-      const createdAt = Date.now()
-      const path = `/chat/${id}`
+      const title = json.messages[0].content.substring(0, 100);
+      const id = json.id ?? nanoid();
+      const createdAt = Date.now();
+      const path = `/chat/${id}`;
       const payload = {
         id,
         title,
@@ -51,17 +55,13 @@ export async function POST(req: Request) {
           ...messages,
           {
             content: completion,
-            role: 'assistant'
-          }
-        ]
-      }
-      await kv.hmset(`chat:${id}`, payload)
-      await kv.zadd(`user:chat:${userId}`, {
-        score: createdAt,
-        member: `chat:${id}`
-      })
-    }
-  })
+            role: 'assistant',
+          },
+        ],
+      };
+     
+    },
+  });
 
-  return new StreamingTextResponse(stream)
+  return new StreamingTextResponse(stream);
 }
--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -6,17 +6,17 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'Explain the meaning of life.',
+    message: `What is the meaning of life?`
   },
-  {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
-  },
-  {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
-  }
+  // {
+  //   heading: 'Summarize an article',
+  //   message: 'Summarize the following article for a 2nd grader: \n'
+  // },
+  // {
+  //   heading: 'Draft an email',
+  //   message: `Draft an email to my boss about the following: \n`
+  // }
 ]
 
 export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
@@ -24,14 +24,11 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Welcome to Chat Guru.
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
           This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
+          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>
           .
         </p>
         <p className="leading-normal text-muted-foreground">
